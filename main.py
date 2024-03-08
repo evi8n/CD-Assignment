98048a363f5791edd4409273ccc818bafbb3b83a
Evi Siafaka <evi.siafaka@hotmail.com>
2024-03-08 22:44:46 +0100

    updated main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Fetch a random quote from the Quotable REST API
+    # Get a random quote from the Quotable REST API
     response = requests.get("https://api.quotable.io/random")
     if response.status_code == 200:
         quote_data = response.json()
@@ -16,13 +16,12 @@ def index():
         quote = "Error fetching quote"
         author = "Unknown"
 
-    # Render the template with the quote and author
     return render_template("index.html", quote=quote, author=author)
 
 
 @app.route("/new_quote")
 def new_quote():
-    # Fetch a new random quote from the Quotable REST API
+    # Get a new random quote from the Quotable REST API
     response = requests.get("https://api.quotable.io/random")
     if response.status_code == 200:
         quote_data = response.json()
